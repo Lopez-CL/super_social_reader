52a1d88d692df4c85926958fcd3dc404d708a0ac
Lopez-CL <cllopez511@gmail.com>
2023-01-16 18:44:01 -0500

    styled dashboard, essentially done with add comic page and creation functionality.

--- flask_app/controllers/comics.py
+++ flask_app/controllers/comics.py
@@ -2,6 +2,7 @@ from flask_app import app
 from flask_app.models import comic,user, comment
 from flask import request, redirect, session, render_template
 
+# Page Renders
 @app.route('/dashboard')
 def render_dash():
     if 'user_id' not in session:
@@ -12,3 +13,32 @@ def render_dash():
             'id': session['user_id']
         }
         return render_template('dashboard.html', this_user = user.User.get_user_by_id(data))
+
+@app.route('/add/comic')
+def render_add_comic_page():
+    if 'user_id' not in session:
+        return redirect('/')
+    else:
+        return render_template('add_comic.html')
+
+#CRUD Logic
+
+@app.route('/create/comic', methods=['post'])
+def add_comic():
+    if not comic.Comic.val_comic(request.form):
+        return redirect('/add/comic')
+    else:
+        data = {
+            'user_id': session['user_id'],
+            'title': request.form['title'],
+            'author': request.form['author'],
+            'artist': request.form['artist'],
+            'colorist': request.form['colorist'],
+            'letterer': request.form['letterer'],
+            'status': request.form['status'],
+            'rating': None,
+            'thought': None
+        }
+        comic.Comic.save(data)
+        return redirect('/dashboard')
+
---
+++ flask_app/models/comic.py
@@ -0,0 +1,55 @@
+from flask_app import app
+from flask import flash
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask_app.models import user, comment
+db_name = 'super_social_reader'
+
+
+class Comic:
+    def __init__(self, data):
+        self.id = data['id']
+        self.user_id = data['user_id']
+        self.author = data['author']
+        self.artist = data['artist']
+        self.colorist = data['colorist']
+        self.letterer = data['letterer']
+        self.rating = data['rating']
+        self.thought = data['thought']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+# CRUD Class methods
+
+    @classmethod
+    def save(cls, data):
+        query = """INSERT INTO comics
+        (user_id, title, author, artist, colorist, letterer, status, rating,thought)
+        VALUES (%(user_id)s,%(title)s,%(author)s,%(artist)s,%(colorist)s,%(letterer)s,%(status)s,%(rating)s,%(thought)s);"""
+        print('Creating comic!')
+        return connectToMySQL(db_name).query_db(query, data)
+
+
+# Static Validations
+
+    @staticmethod
+    def val_comic(comic_data):
+        is_valid = True
+        if len(comic_data['title']) <= 0:
+            flash('*Title required. Must provide title of comic', 'comic')
+            is_valid = False
+        if len(comic_data['author']) <= 0:
+            flash('*Author required. Must enter name(s)', 'comic')
+            is_valid = False
+        if len(comic_data['artist']) <= 0:
+            flash('*Artist required. Must enter name(s)', 'comic')
+            is_valid = False
+        if len(comic_data['colorist']) <= 0:
+            flash('*Colorist required. Must enter name(s)', 'comic')
+            is_valid = False
+        if len(comic_data['letterer']) <= 0:
+            flash('*Letterer required. Must enter name(s)', 'comic')
+            is_valid = False
+        if len(comic_data['status']) <= 0:
+            flash('*You must indicate reading status', 'comic')
+            is_valid = False
+        return is_valid
