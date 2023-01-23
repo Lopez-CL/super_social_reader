7f1dd4f51ba543093e2e4b70e49ef997d29fbd7d
Lopez-CL <cllopez511@gmail.com>
2023-01-22 22:12:30 -0500

    fixing character count JS for update comic, added user profile image

--- flask_app/static/script.js
+++ flask_app/static/script.js
@@ -21,7 +21,20 @@ $(document).ready(()=>{
         let remaining = 255 - post.length;
         remaining <= 0? $('.count').css({
             color: 'red'
-        }):''
+        }):$('.count').css({
+            color: 'black'
+        })
         $('.characters').html(remaining);
+    })
+    $('.comment-text').on('keyup', event =>{
+        $('comment-text').focus();
+        let post = $(event.currentTarget).val();
+        let remaining = 255 - post.length;
+        remaining <= 0? $('.comment-count').css({
+            color: 'red'
+        }):$('.comment-count').css({
+            color: 'black'
+        })
+        $('.comment-characters').html(remaining);
     })
 })
