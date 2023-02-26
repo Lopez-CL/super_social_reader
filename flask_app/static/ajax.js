09ca72ccf83316e78c3307e1a4dccb6720c43d02
Lopez-CL <cllopez511@gmail.com>
2023-02-25 22:59:06 -0500

    character card feature fully style

--- flask_app/static/ajax.js
+++ flask_app/static/ajax.js
@@ -7,20 +7,22 @@ const superAPI = e => {
     .then(data =>{
         console.log(data) 
         data.error && console.log("Couldn't find by that name")
-        if(data.results.length > 3){
+        if(data.results.length > 10){
             return console.log('Too many results matched your query. Try again')}
         const container = document.getElementById('character-container')
         container.innerHTML = "";
-        for(let i = 0; i < data.results.length; i++){
+        for(let i = 0; i < 5; i++){
             let cardInfo = document.createElement('div');
-            cardInfo.classList.add('card', 'bg-warning', 'me-5', 'text-dark', 'comics', 'details', 'border', 'border-border-dark', 'current-background');
+            cardInfo.classList.add('card', 'bg-warning', 'me-5', 'mt-4', 'text-dark', 'comics', 'details', 'border', 'border-2', 'border-dark', 'current-background');
             cardInfo.style.width = "18rem"
             let image = document.createElement('img');
             image.classList.add('card-img-top');
             image.src = data.results[i].image.url
             let characterInfo = document.createElement('ul')
             characterInfo.classList.add('list-group', 'list-group-flush' );
-            characterInfo.innerHTML =`<li class="list-group-item list-group-item-action">Name: ${data.results[i].name}</li>`
+            characterInfo.innerHTML =`<li class="list-group-item list-group-item-action">Name: ${data.results[i].name}/${data.results[i].biography["full-name"]}</li> \n
+            <li class="list-group-item list-group-item-action">First Appearance (Comic): ${data.results[i].biography["first-appearance"]}</li>
+            <li class="list-group-item list-group-item-action">Publisher: ${data.results[i].biography.publisher}</li>`
             cardInfo.appendChild(image)
             cardInfo.appendChild(characterInfo)
             container.appendChild(cardInfo)
